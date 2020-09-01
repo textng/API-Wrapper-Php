b7f887568c40ec5da08e5e39baa9a315b5458398
Textng Communications <66287225+textng@users.noreply.github.com>
2020-09-01 20:11:52 +0100

    Add files via upload

---
+++ src/Enum/Param.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Elmage\TextNg\Enum;
+
+class Param
+{
+    const API_KEY = 'key';
+    const CHECK_BALANCE = 'checkbalance';
+    const ROUTE = 'route';
+    const MESSAGE = 'message';
+    const SENDER = 'sender';
+    const TYPE = 'type';
+    const SCHEDULE = 'schedule';
+    const BYPASSCODE = 'bypasscode';
+    const PHONE = 'phone';
+    const REFERENCE = 'reference';
+    const REQ = 'req';
+    const USED_ROUTE = 'used_route';
+
+    const CUSTOMER_NAME = 'customername';
+    const CUSTOMER_PHONE = 'customerphone';
+    const CATEGORY_ID = 'catid';
+}
